646bc6bd9a65e0257f758a2341e0734ea0366751
Murilo Menezes Mendonca <99814079@ambev.com.br>
2020-11-10 21:40:47 -0300

    consuming endpoints + swagger

--- starter_files/endpoint.py
+++ starter_files/endpoint.py
@@ -3,9 +3,9 @@ import json
 
 # URL for the web service, should be similar to:
 # 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
-scoring_uri = ''
+scoring_uri = 'http://16a74140-607f-4502-afee-34ad41ebd5fa.eastus.azurecontainer.io/score'
 # If the service is authenticated, set the key or token
-key = ''
+key = 'KawJLgH1HeVkb6My1Ydw4ckrng0EqefP'
 
 # Two sets of data to score, so we get two results back
 data = {"data":
